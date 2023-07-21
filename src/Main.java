f65cba3f783f4fe6dd35ac163242690e00f4ad50
HFeeney <haydenbfeeney@gmail.com>
2023-07-21 08:22:50 -0700

    Restrict HPS to process high amplitude, proper frequencies

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,24 @@ public class Main extends PApplet {
 
     FFT fft; // fast fourier transform object
     AudioIn in; // audio input source
-    int bands = 16384; // the number of frequency bands to decompose signal into
-    float[] spectrum = new float[bands]; // bins for each frequency band recording amplitude
+    Amplitude volume;
+    static final int BANDS = 16384; // the number of frequency bands to decompose signal into
+    float[] spectrum = new float[BANDS]; // bins for each frequency band recording amplitude
     static final int SAMPLE_RATE = 44100; // standard microphone sample rate in Hz
     static final int MAX_FREQUENCY = SAMPLE_RATE / 2; // most precise frequency detectable is half sample rate
-    double resolution = (double) MAX_FREQUENCY / bands; // width of each frequency band in Hz
+    static final double RESOLUTION = (double) MAX_FREQUENCY / BANDS; // width of each frequency band in Hz
 
-    static final int HARMONICS = 3; // number of harmonics to consider in product
-    float[] hpSpectrum = new float[bands]; // to store product of harmonics of each frequency band
+    static final int HARMONICS = 2; // number of harmonics to consider in product
+    float[] hpSpectrum = new float[BANDS]; // to store product of harmonics of each frequency band
     static final float LOW_FREQ_NOISE_CUTOFF = 50f; // below this value, frequencies considered noise
+    static final float HIGH_FREQ_NOISE_CUTOFF = 1200f; // above this value, frequencies considered noise
+    static final int HPS_START_BIN = (int)(LOW_FREQ_NOISE_CUTOFF / RESOLUTION);
+    static final int HPS_END_BIN = (int)(HIGH_FREQ_NOISE_CUTOFF / RESOLUTION);
+    static final float MIN_VOLUME = 0.01f; // lowest volume for which pitch will be reported
 
     public void settings() {
         size(1800, 360);
-        System.out.println(resolution);
+        System.out.println(RESOLUTION);
     }
 
     public void setup() {
@@ -26,14 +31,16 @@ public class Main extends PApplet {
         /* START CODE FROM PROCESSING WEBSITE */
 
         // Create an Input stream which is routed into the Amplitude analyzer
-        fft = new FFT(this, bands);
+        fft = new FFT(this, BANDS);
         in = new AudioIn(this, 0);
+        volume = new Amplitude(this);
 
         // start the Audio Input
         in.start();
 
         // patch the AudioIn
         fft.input(in);
+        volume.input(in);
 
         /* END CODE FROM PROCESSING WEBSITE */
     }
@@ -42,36 +49,36 @@ public class Main extends PApplet {
         background(255);
         fft.analyze(spectrum);
 
+        stroke(0);
+
         // fft output
-        for(int i = 0; i < bands; i++){
+        for(int i = 0; i < BANDS; i++){
             // The result of the FFT is normalized
             // draw the line for frequency band i scaling it up by 5 to get more amplitude.
             line(i, height, i, height - spectrum[i]*height*5 );
         }
 
-        // start search above 50 hz bin
-        int startBin = (int) (LOW_FREQ_NOISE_CUTOFF / resolution);
 
-        // update harmonic product spectrum
-        for (int i = startBin; i < spectrum.length; i++) {
-            // compute product of amplitudes of each bin's harmonics
-            float product = 1.0f;
-            for (int j = 1; j <= HARMONICS && j * i < spectrum.length; j++) {
-                product *= spectrum[j * i];
+        if (volume.analyze() > MIN_VOLUME) {
+            // update harmonic product spectrum, and find max bucket
+            // start and end search at lowest and highest acceptable frequencies
+            int maxBucket = 0;
+            for (int i = HPS_START_BIN; i < HPS_END_BIN; i++) {
+                // compute product of amplitudes of each bin's harmonics
+                float product = 1.0f;
+                for (int j = 1; j <= HARMONICS && j * i < spectrum.length; j++) {
+                    product *= spectrum[j * i];
+                }
+                hpSpectrum[i] = product;
+                if (hpSpectrum[maxBucket] < hpSpectrum[i]) {
+                    maxBucket = i;
+                }
             }
-            hpSpectrum[i] = product;
-        }
 
-        // find maximum value
-        int maxIndex = 0;
-        for (int i = 1; i < hpSpectrum.length; i++) {
-            if (hpSpectrum[maxIndex] < hpSpectrum[i]) {
-                maxIndex = i;
-            }
+            System.out.println(maxBucket * RESOLUTION);
+            stroke(100, 255, 250);
+            line(maxBucket, height, maxBucket, 0);
         }
-        System.out.println(maxIndex * resolution);
-        stroke(255, 0, 0);
-        line(maxIndex, height, maxIndex, height - hpSpectrum[maxIndex]*height*5000);
     }
     public static void main(String[] args) {
         PApplet.main("Main");
